505d0ea7900ef09765e5f7d4e40adb4573273730
Void-Great-Emperor <akshaykumarsb177013@gmail.com>
2023-01-09 13:21:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ def get_user_list(config, key):
 class Config(object):
     LOGGER = True
 
-    API_ID = int(getenv("API_ID", "21927988"))
-    API_HASH = getenv("API_HASH", "e18f720acdff1e5b0ec80616aecd8a5a")
+    API_ID = int(getenv("API_ID", "24833791"))
+    API_HASH = getenv("API_HASH", "42488cb247a33d13d5f97d6839c8e52b)
     TOKEN = getenv("TOKEN", "5881951624:AAE9LaaJGXbUIs_xmZGiu4pkHBWNY5_Ip-4")
     OWNER_ID = int(getenv("OWNER_ID", "2064735436"))
     OWNER_USERNAME = getenv("OWNER_USERNAME", "plumblossomsword")
