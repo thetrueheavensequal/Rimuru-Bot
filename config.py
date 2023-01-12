b10e42bcabc9e89ccc8abc59a52c74f9a42c7910
Akshay <jinmori58690@gmail.com>
2023-01-12 15:40:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,16 +15,16 @@ def get_user_list(config, key):
 class Config(object):
     LOGGER = True
 
-    API_ID = int(getenv("API_ID", "24833791"))
-    API_HASH = getenv("API_HASH", "42488cb247a33d13d5f97d6839c8e52b")
-    TOKEN = getenv("TOKEN", "5881951624:AAE9LaaJGXbUIs_xmZGiu4pkHBWNY5_Ip-4")
+    API_ID = int(getenv("API_ID", "21927988"))
+    API_HASH = getenv("API_HASH", "e18f720acdff1e5b0ec80616aecd8a5ab")
+    TOKEN = getenv("TOKEN", "5881951624:AAFydla8mLAvfXfhYJTIPmbIswo-pUEv9ns")
     OWNER_ID = int(getenv("OWNER_ID", "2064735436"))
     OWNER_USERNAME = getenv("OWNER_USERNAME", "plumblossomsword")
     SUPPORT_CHAT = "theoneandonlymurim"
     EVENT_LOGS = int(getenv("EVENT_LOGS", "-1001859171071"))
     MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://plumblossomsword:Qn57AqxG3GRIu9IP@komi.zc6e9si.mongodb.net/?retryWrites=true&w=majority")
     DB_NAME = getenv("DB_NAME", "EXON_2")
-    DATABASE_URL = getenv("DATABASE_URL", "postgresql://plumblossomsword:jmppEmTPgd5lJYoAZ039qKvxoS3hdBDI@dpg-cetvhvla4990mi84rpug-a.singapore-postgres.render.com/telegram_wbmv")
+    DATABASE_URL = getenv("DATABASE_URL", "postgresql://plumblossomsword:YOHMSYTKcjYLaqioWlyCDRrJZSkSmvQ3@dpg-cevf5782i3mntl1sh6q0-a.frankfurt-postgres.render.com/telegram_postgresql")
 
     # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
     if DATABASE_URL.startswith("postgres://"):
